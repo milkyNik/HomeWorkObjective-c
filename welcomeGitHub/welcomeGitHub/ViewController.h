12cebb82441005f608ee8cc76aa02f8cac52fe8c
milkyNik <milniklaus@gmail.com>
2015-02-06 10:56:29 +0200

    test

---
+++ welcomeGitHub/welcomeGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  welcomeGitHub
+//
+//  Created by iMac on 06.02.15.
+//  Copyright (c) 2015 MilNik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ welcomeGitHub/welcomeGitHub/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  welcomeGitHub
+//
+//  Created by iMac on 06.02.15.
+//  Copyright (c) 2015 MilNik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
